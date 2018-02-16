c7917312e7a1d95e74607c975c5be5047066d00e
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-16 10:07:13 -0800

    write second spec for save function

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -3,7 +3,15 @@ require('word_definer')
 require('pry')
 
 describe('word_definer') do
-  it('will check if the array is empty') do
-    expect(Word.all()).to(eq([]))
- end
+  describe('.all') do
+    it('will check if the array is empty') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('save')do
+    it('will save an inputted word into list')do
+      expect(Word.save()).to(eq([word]))
+    end
+  end
 end
