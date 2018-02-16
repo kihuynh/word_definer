4e6361ce15bfcaf4894da113cdff7c76f3229842
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-16 10:54:54 -0800

    write third spec and fail

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -3,17 +3,28 @@ require('word_definer')
 require('pry')
 
 describe('word_definer') do
+
+
   describe('.all') do
     it('will check if the array is empty') do
       expect(Word.all()).to(eq([]))
     end
   end
 
-  describe('save')do
+  describe('save') do
     it('will save inputted word into list')do
       vocab = Word.new(:word => "Spleen")
       vocab.save()
       expect(Word.all()).to(eq([vocab]))
+    end
+  end
+
+  describe('clear') do
+    it('will clear out the array after saving inputs. prevent duplicates in the array?') do
+    vocab = Word.new(:word => "Justice")
+    vocab.save()
+    Word.clear()
+    expect(Word.all()).to(eq([]))
     end
   end
 end
